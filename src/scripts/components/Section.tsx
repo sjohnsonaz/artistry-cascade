1449991c2046771f0eab0f4cea1bf40d3a1832d8
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-12-31 12:55:24 -0700

    Added locked to Section.

--- src/scripts/components/Section.tsx
+++ src/scripts/components/Section.tsx
@@ -2,6 +2,7 @@ import Cascade, { Component, Elements } from 'cascade';
 
 export interface ISectionProps extends Elements.JSXElement {
     title: any;
+    locked?: boolean;
 }
 
 export default class Section extends Component<ISectionProps> {
@@ -9,10 +10,18 @@ export default class Section extends Component<ISectionProps> {
         let classNames = this.props.className ? [this.props.className] : [];
         classNames.push('section');
         let className = classNames.join(' ');
+
+        let innerClassNames = ['section-content'];
+        if (this.props.locked) {
+            innerClassNames.push('lock-contents');
+            innerClassNames.push('locked');
+        }
+
+        let innerClassName = innerClassNames.join(' ');
         return (
             <section className={className} {...this.props}>
                 <header>{this.props.title}</header>
-                <div className="section-content">{this.children}</div>
+                <div className={innerClassName}>{this.children}</div>
             </section>
         );
     }
