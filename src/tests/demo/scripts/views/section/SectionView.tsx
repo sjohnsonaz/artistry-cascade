3ec99d4a699ce0fa921fa55e8dde4e19ec24489b
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-12-31 13:07:13 -0700

    Added lock button to SectionView.

--- src/tests/demo/scripts/views/section/SectionView.tsx
+++ src/tests/demo/scripts/views/section/SectionView.tsx
@@ -1,18 +1,26 @@
-import Cascade, { Component } from 'cascade';
+import Cascade, { Component, observable } from 'cascade';
 
-import { Section } from '../../../../../scripts/modules/CascadeComponents';
+import { Button, Section } from '../../../../../scripts/modules/CascadeComponents';
 
 export interface ISectionViewProps {
 
 }
 
 export default class SectionView extends Component<ISectionViewProps> {
+    @observable locked: boolean = false;
+    lockContents = () => {
+        this.locked = true;
+        window.setTimeout(() => {
+            this.locked = false;
+        }, 3000);
+    }
     render() {
         return (
             <div>
                 <h2>Section</h2>
-                <Section title="Section">
-                    Section Content
+                <Section title="Section" locked={this.locked}>
+                    Section Content<br />
+                    <Button onclick={this.lockContents}>Lock</Button>
                 </Section>
             </div>
         );
