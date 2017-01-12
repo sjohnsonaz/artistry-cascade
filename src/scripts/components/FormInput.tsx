349881980ee97dd9911796a76c9028be2f53d5dd
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-01-12 13:32:50 -0700

    Fixed FormInput.

--- src/scripts/components/FormInput.tsx
+++ src/scripts/components/FormInput.tsx
@@ -1,11 +1,18 @@
-import Cascade, {Component, VirtualNode, IVirtualNode, IVirtualNodeProps} from 'cascade';
+import Cascade, { Component, Elements } from 'cascade';
 
-export interface IFormInputProps {
-
+export interface IFormInputProps extends Elements.JSXInputElement {
+    model: Object;
+    modelProp: string;
 }
 
 export class FormInput extends Component<IFormInputProps> {
+    oninput = (event?: Event) => {
+        let {model, modelProp} = this.props;
+        model[modelProp] = (event.target as HTMLInputElement).value;
+    }
+
     render() {
-        return <input {...this.props} />
+        let {model, modelProp} = this.props;
+        return <input {...this.props} value={model[modelProp]} oninput={this.oninput} />
     }
 }
