b4d1a7583196b282974024d2bc4742339f42acb0
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-01-16 17:10:25 -0700

    Added ref callbacks to Carousel.

--- src/scripts/components/Carousel.tsx
+++ src/scripts/components/Carousel.tsx
@@ -6,17 +6,34 @@ export interface ICarouselProps {
 }
 
 export default class Carousel extends Component<ICarouselProps> {
+    container: HTMLElement;
+    child: HTMLElement;
+
+    containerRef = (container: HTMLElement) => {
+        this.container = container;
+    }
+
+    childRef = (child: HTMLElement) => {
+        this.child = child;
+    }
+
     render() {
         let {activeIndex} = this.props;
 
         let classNames = this.props.className ? [this.props.className] : [];
         classNames.push('carousel');
+        classNames.push('carousel-run');
 
         activeIndex = activeIndex || 0;
 
         let className = classNames.join(' ');
+
+        window.setTimeout(() => {
+            this.container.classList.remove('carousel-run');
+        }, 500);
+
         return (
-            <div className={className} style="background-color: black">
+            <div className={className} style="background-color: black" ref={this.containerRef}>
                 {this.children ? this.children.map((child, index) => {
                     let className = activeIndex === index ? 'carousel-selected' : '';
                     return <div className={className}>{child}</div>
