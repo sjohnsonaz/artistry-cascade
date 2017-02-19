e09fb894b8d8896f06d8f9639a324372020f0e9c
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-02-19 16:13:14 -0700

    Updated Section closing.

--- src/scripts/components/Section.tsx
+++ src/scripts/components/Section.tsx
@@ -1,21 +1,68 @@
-import Cascade, { Component, Elements } from 'cascade';
+import Cascade, { Component, Elements, observable } from 'cascade';
+
+import Button from './Button';
 
 export interface ISectionProps extends Elements.JSXElement {
     title: any;
     lockable?: boolean;
     locked?: boolean;
+    closeable?: boolean;
     closed?: boolean;
+    onClose?: (closed: boolean) => void;
 }
 
 export default class Section extends Component<ISectionProps> {
+    @observable closed: boolean;
+
+    close = () => {
+        if (this.props.onClose) {
+            // Get closed value
+            let closed = typeof this.props.closed !== 'undefined' ?
+                this.props.closed :
+                (this.closed || false);
+            this.props.onClose(closed);
+        } else {
+            this.closed = !this.closed;
+        }
+    }
+
+    afterRender(node: HTMLElement) {
+        // Get closed value
+        let closed = typeof this.props.closed !== 'undefined' ?
+            this.props.closed :
+            (this.closed || false);
+
+        // Clear toggleTimeout
+        let toggleTimeout: number = (node as any).toggleTimeout;
+        if (typeof toggleTimeout === 'number') {
+            window.clearTimeout(toggleTimeout);
+            (node as any).toggleTimeout = undefined;
+        }
+
+        let header = node.childNodes[0] as HTMLElement;
+        let content = node.childNodes[1] as HTMLElement;
+
+        if (closed) {
+            node.style.height = node.offsetHeight + 'px';
+            node.style.height = header.offsetHeight + 'px';
+        } else {
+            var sectionBorder = node.offsetHeight - node.clientHeight;
+            node.style.height = sectionBorder / 2 + header.offsetHeight + content.offsetHeight + 'px';
+            (node as any).toggleTimeout = window.setTimeout(function () {
+                node.style.height = 'auto';
+            }, 220)
+        }
+    }
+
     render() {
         let classNames = this.props.className ? [this.props.className] : [];
         classNames.push('section');
 
+        /*
         if (this.props.closed) {
             classNames.push('section-closed');
         }
-
+        */
         let innerClassNames = ['section-content'];
         if (this.props.lockable) {
             innerClassNames.push('lock-contents');
@@ -28,7 +75,12 @@ export default class Section extends Component<ISectionProps> {
         let innerClassName = innerClassNames.join(' ');
         return (
             <section className={className} {...this.props}>
-                <header>{this.props.title}</header>
+                <header>
+                    {this.props.title}
+                    {this.props.closeable ?
+                        <Button className="section-toggle" onclick={this.close}>-</Button>
+                        : undefined}
+                </header>
                 <div className={innerClassName}>{this.children}</div>
             </section>
         );
--- src/tests/demo/scripts/views/section/SectionView.tsx
+++ src/tests/demo/scripts/views/section/SectionView.tsx
@@ -8,15 +8,22 @@ export interface ISectionViewProps {
 
 export default class SectionView extends Component<ISectionViewProps> {
     @observable locked: boolean = false;
+    @observable closed: boolean = false;
+
+    close = () => {
+        this.closed = !this.closed;
+    }
+
     lockContents = () => {
         this.locked = true;
         window.setTimeout(() => {
             this.locked = false;
         }, 1000);
     }
+
     render() {
         return (
-            <Section title="Section" lockable locked={this.locked}>
+            <Section title="Section" lockable closeable locked={this.locked} closed={this.closed} onClose={this.close}>
                 Section Content<br />
                 <Button onclick={this.lockContents}>Lock</Button>
             </Section>
