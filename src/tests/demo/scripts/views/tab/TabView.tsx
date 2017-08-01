8093b5b22bcce2159d3b53a836ed01656eb6556b
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-01 16:26:14 -0700

    Fixed TabView input classes.

--- src/tests/demo/scripts/views/tab/TabView.tsx
+++ src/tests/demo/scripts/views/tab/TabView.tsx
@@ -12,18 +12,18 @@ export default class TabView extends Component<ITabViewProps> {
             <Section title="Tab">
                 <Tab titles={['Tab 1', 'Tab 2', 'Tab 3']} animated>
                     <div>Tab 1 Content<br />
-                        <input type="text" />
+                        <input type="text" className="input" />
                     </div>
                     <div>
                         Tab 2 Content<br />
                         More Content<br />
-                        <input type="text" />
+                        <input type="text" className="input" />
                     </div>
                     <div>
                         Tab 3 Content<br />
                         Event more Content<br />
                         Another line of Content<br />
-                        <input type="text" />
+                        <input type="text" className="input" />
                     </div>
                 </Tab>
             </Section>
