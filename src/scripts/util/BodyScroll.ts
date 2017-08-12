af14920a3e6b92e487f9c4bc9d9d99b53ef3dedf
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-12 01:02:34 -0700

    Added notes to the scrollTop fix.

--- src/scripts/util/BodyScroll.ts
+++ src/scripts/util/BodyScroll.ts
@@ -3,6 +3,7 @@ export default class BodyScroll {
         let body = document.body;
         let root = document.querySelector('body > .root');
         if (root) {
+            // We must query multiple objects for the scrollTop.
             if (lock) {
                 let scrollTop = window.pageYOffset || document.documentElement.scrollTop || body.scrollTop || root.scrollTop;
                 body.classList.add('body-scroll-lock');
@@ -10,6 +11,7 @@ export default class BodyScroll {
             } else {
                 let scrollTop = root.scrollTop || window.pageYOffset || document.documentElement.scrollTop || body.scrollTop;
                 body.classList.remove('body-scroll-lock');
+                // We must set both of these for Chrome and Firefox.
                 body.scrollTop = scrollTop;
                 document.documentElement.scrollTop = scrollTop;
             }
