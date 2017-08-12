8c83778aeedd42c759ba00556214f17f02463259
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-11 21:51:13 -0700

    Fixed BodyScroll for repeat calls.

--- src/scripts/util/BodyScroll.ts
+++ src/scripts/util/BodyScroll.ts
@@ -4,11 +4,11 @@ export default class BodyScroll {
         let root = document.querySelector('body > .root');
         if (root) {
             if (lock) {
-                let scrollTop = body.scrollTop;
+                let scrollTop = body.scrollTop || root.scrollTop;
                 body.classList.add('body-scroll-lock');
                 root.scrollTop = scrollTop;
             } else {
-                let scrollTop = root.scrollTop;
+                let scrollTop = root.scrollTop || body.scrollTop;
                 body.classList.remove('body-scroll-lock');
                 body.scrollTop = scrollTop;
             }
