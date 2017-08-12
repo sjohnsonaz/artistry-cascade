9e511c5c271af467643a901ab4bca277e625edf0
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-12 01:01:11 -0700

    Fixed scroll locking for Firefox.

--- src/scripts/util/BodyScroll.ts
+++ src/scripts/util/BodyScroll.ts
@@ -4,13 +4,14 @@ export default class BodyScroll {
         let root = document.querySelector('body > .root');
         if (root) {
             if (lock) {
-                let scrollTop = body.scrollTop || root.scrollTop;
+                let scrollTop = window.pageYOffset || document.documentElement.scrollTop || body.scrollTop || root.scrollTop;
                 body.classList.add('body-scroll-lock');
                 root.scrollTop = scrollTop;
             } else {
-                let scrollTop = root.scrollTop || body.scrollTop;
+                let scrollTop = root.scrollTop || window.pageYOffset || document.documentElement.scrollTop || body.scrollTop;
                 body.classList.remove('body-scroll-lock');
                 body.scrollTop = scrollTop;
+                document.documentElement.scrollTop = scrollTop;
             }
         }
     }
