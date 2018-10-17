ebf1b7ccea063fe5c58223269b83dddd1dbc14cd
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-10-16 17:48:17 -0700

    Added MaskedInput and TimeInput Components.

---
+++ src/scripts/components/MaskedInput.tsx
@@ -0,0 +1,226 @@
+import Cascade, { Component, observable, Ref } from 'cascade';
+
+import Mask, { ISelection, KeyboardMovement, IMaskUpdate } from '../util/Mask';
+
+export interface IMaskedInputProps<T> extends Partial<HTMLInputElement> {
+    id?: string;
+    className?: string;
+    mask: string;
+    onchange?: (event: Event) => (void | boolean);
+    value?: any;
+}
+
+export default class MaskedInput<T> extends Component<IMaskedInputProps<T>> {
+    inputRef: Ref<HTMLInputElement> = new Ref();
+    command: boolean = false;
+    mask: Mask;
+
+    @observable value?: string;
+    @observable selectionStart?: number = 0;
+    @observable selectionEnd?: number = 0;
+
+    constructor(props: IMaskedInputProps<T>, children: any[]) {
+        super(props, children);
+        this.mask = new Mask(this.props.mask);
+        this.value = this.mask.formatValue(this.props.value, true);
+    }
+
+    updateElement(maskUpdate: IMaskUpdate) {
+        let input = this.inputRef.current;
+        if (typeof maskUpdate.value !== 'undefined') {
+            input.value = maskUpdate.value;
+            this.value = maskUpdate.value;
+        }
+        if (typeof maskUpdate.selectionEnd !== 'undefined' && typeof maskUpdate.selectionEnd !== 'undefined') {
+            input.setSelectionRange(maskUpdate.selectionStart, maskUpdate.selectionEnd, 'none');
+            this.selectionStart = maskUpdate.selectionStart;
+            this.selectionEnd = maskUpdate.selectionEnd;
+        }
+    }
+
+    beforeRender(mounted?: boolean) {
+        if (mounted) {
+            let {
+                value
+            } = this.props;
+            if (this.prevProps.value !== value) {
+                try {
+                    this.updateElement(this.mask.updateValue(
+                        value,
+                        this.value
+                    ));
+                }
+                catch (e) {
+                    this.rollback();
+                }
+            }
+        }
+    }
+
+    onFocus = (event?: FocusEvent) => {
+        event.preventDefault();
+        this.updateElement(this.mask.updateSelection(
+            (event.target as HTMLInputElement).value,
+            this.getSelection()
+        ));
+    }
+
+    onBlur = () => {
+        this.command = false;
+    }
+
+    onClick = (event?: MouseEvent) => {
+        event.preventDefault();
+        this.updateElement(this.mask.updateSelection(
+            (event.target as HTMLInputElement).value,
+            this.getSelection()
+        ));
+    }
+
+    onSelect = (event?: MouseEvent) => {
+        event.preventDefault();
+        this.updateElement(this.mask.updateSelection(
+            (event.target as HTMLInputElement).value,
+            this.getSelection()
+        ));
+    }
+
+    onChange = (event?: Event) => {
+        event.preventDefault();
+        try {
+            this.updateElement(this.mask.updateValue(
+                (event.target as HTMLInputElement).value,
+                this.value
+            ));
+            if (this.props.onchange) {
+                this.props.onchange(event);
+            }
+        }
+        catch (e) {
+            this.rollback();
+        }
+    }
+
+    onKeyDown = (event: KeyboardEvent) => {
+        switch (event.keyCode) {
+            case 8: // Backspace
+                event.preventDefault();
+                try {
+                    this.updateElement(this.mask.deleteCharacter(
+                        (event.target as HTMLInputElement).value,
+                        this.getSelection(),
+                        false
+                    ));
+                }
+                catch (e) {
+                    this.rollback();
+                }
+                break;
+            case 35: // End
+                event.preventDefault();
+                this.updateElement(this.mask.updateSelection(
+                    (event.target as HTMLInputElement).value,
+                    this.getSelection(),
+                    KeyboardMovement.end
+                ));
+                break;
+            case 36: // Home
+                event.preventDefault();
+                this.updateElement(this.mask.updateSelection(
+                    (event.target as HTMLInputElement).value,
+                    this.getSelection(),
+                    KeyboardMovement.home
+                ));
+                break;
+            case 37: // Left
+                event.preventDefault();
+                this.updateElement(this.mask.updateSelection(
+                    (event.target as HTMLInputElement).value,
+                    this.getSelection(),
+                    this.command ? KeyboardMovement.home : KeyboardMovement.left
+                ));
+                break;
+            case 39: // Right
+                event.preventDefault();
+                this.updateElement(this.mask.updateSelection(
+                    (event.target as HTMLInputElement).value,
+                    this.getSelection(),
+                    this.command ? KeyboardMovement.end : KeyboardMovement.right
+                ));
+                break;
+            case 46: // Delete
+                event.preventDefault();
+                try {
+                    this.updateElement(this.mask.deleteCharacter(
+                        (event.target as HTMLInputElement).value,
+                        this.getSelection(),
+                        true
+                    ));
+                }
+                catch (e) {
+                    this.rollback();
+                }
+                break;
+            case 91: // Command Left
+            case 93: // Command Right
+            case 224: // Command - Firefox
+                this.command = true;
+                break;
+        }
+    }
+
+    onKeyUp = (event: KeyboardEvent) => {
+        switch (event.keyCode) {
+            case 91: // Command Left
+            case 93: // Command Right
+            case 224: // Command - Firefox
+                this.command = false;
+                break;
+        }
+    }
+
+    getSelection(): ISelection {
+        let input = this.inputRef.current;
+        return {
+            start: input.selectionStart,
+            end: input.selectionEnd,
+            direction: input.selectionDirection as any
+        };
+    }
+
+    rollback() {
+        this.updateElement({
+            value: this.value,
+            selectionStart: this.selectionStart,
+            selectionEnd: this.selectionEnd
+        });
+    }
+
+    render() {
+        let {
+            id,
+            className,
+            value,
+            onchange,
+            ...props
+        } = this.props;
+
+        let classNames = this.props.className ? [this.props.className] : [];
+        classNames.push('input');
+
+        return (
+            <input
+                ref={this.inputRef}
+                id={id}
+                className={classNames.join(' ')}
+                onFocus={this.onFocus}
+                onClick={this.onClick}
+                onSelect={this.onSelect}
+                onChange={this.onChange}
+                onKeyDown={this.onKeyDown}
+                onKeyUp={this.onKeyUp}
+                {...props as any}
+            />
+        );
+    }
+}
--- src/tests/demo/scripts/views/form/FormView.tsx
+++ src/tests/demo/scripts/views/form/FormView.tsx
@@ -1,6 +1,6 @@
 import Cascade, { Component } from 'cascade';
 
-import { Button, Form, FormAction, FormContainer, FormDivider, FormInput, Section } from '../../../../../scripts/modules/ArtistryCascade';
+import { Button, Form, FormAction, FormContainer, FormDivider, FormInput, FormText, Section, TimeInput } from '../../../../../scripts/modules/ArtistryCascade';
 
 export interface IFormViewProps {
 
@@ -22,7 +22,66 @@ export default class FormView extends Component<IFormViewProps> {
                     }}
                 >
                     <FormContainer label="Value">
-                        <FormInput fill />
+                        <FormInput type="text" fill />
+                    </FormContainer>
+                    <FormDivider />
+                    <FormText>
+                        <h2>Masked Inputs</h2>
+                    </FormText>
+                    <FormContainer label="Time">
+                        <FormInput mask="[[0-23]]:[[0-59]]" fill />
+                    </FormContainer>
+                    <FormContainer label="Phone">
+                        <FormInput mask="(999) 999-9999" fill />
+                    </FormContainer>
+                    <FormContainer label="Color">
+                        <FormInput mask="#000000" fill />
+                    </FormContainer>
+                    <FormDivider />
+                    <FormText>
+                        <h3>Test Examples</h3>
+                    </FormText>
+                    <FormContainer label="99999">
+                        <FormInput mask="99999" fill />
+                    </FormContainer>
+                    <FormContainer label="aaaaa">
+                        <FormInput mask="aaaaa" fill />
+                    </FormContainer>
+                    <FormContainer label="nnnnn">
+                        <FormInput mask="nnnnn" fill />
+                    </FormContainer>
+                    <FormContainer label="[[0-23]]00000">
+                        <FormInput mask="[[0-23]]00000" fill />
+                    </FormContainer>
+                    <FormContainer label="99aa99aa99aa">
+                        <FormInput mask="99aa99aa99aa" fill />
+                    </FormContainer>
+                    <FormDivider />
+                    <FormText>
+                        <h3>Advanced Input</h3>
+                    </FormText>
+                    <FormContainer label="Time">
+                        <TimeInput
+                            fill
+                            value={(new Date(Date.now())).toUTCString()}
+                            onchange={(event, date?: Date) => {
+                                if (date) {
+                                    console.log(date);
+                                }
+                            }}
+                        />
+                    </FormContainer>
+                    <FormContainer label="Time with Seconds">
+                        <TimeInput
+                            fill
+                            seconds
+                            value={(new Date(Date.now())).toUTCString()}
+                            onchange={(event, date?: Date) => {
+                                if (date) {
+                                    console.log(date);
+                                }
+                            }}
+                        />
                     </FormContainer>
                     <FormDivider />
                     <FormAction>
