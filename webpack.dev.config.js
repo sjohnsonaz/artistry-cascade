7b96bb67bf96af767bed5a7a3d4501c31e4b56ae
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-02-27 23:14:45 -0700

    Fixed sourcemap.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,7 +11,7 @@ module.exports = {
         libraryTarget: 'var',
         library: '[name]'
     },
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
