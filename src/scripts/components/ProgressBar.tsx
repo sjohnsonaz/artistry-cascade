e4ed6f039d5add3c9559f644b1eeb89d99796253
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-02-28 14:57:16 -0700

    Added type, showPercentage prop to ProgressBar. Added min, max, decimal, and decimalFixed props to ProgressBar. Updated ProgressBar to lates artistry spec.

--- src/scripts/components/ProgressBar.tsx
+++ src/scripts/components/ProgressBar.tsx
@@ -1,34 +1,109 @@
 import Cascade, { Component, Elements } from 'cascade';
 
+export type ProgressBarType = 'default' | 'success' | 'info' | 'warning' | 'danger';
+
 export interface IProgressBarProps {
-    className?: string;
     id?: string;
-    value: number;
-    title?: any;
+    className?: string;
+    value?: number;
+    min?: number;
+    max?: number;
+    showPercentage?: boolean;
+    decimal?: number;
+    decimalFixed?: boolean;
+    type?: ProgressBarType;
 }
 
 export default class ProgressBar extends Component<IProgressBarProps> {
     render() {
-        let classNames = this.props.className ? [this.props.className] : [];
+        let {
+            id,
+            className,
+            value,
+            min,
+            max,
+            showPercentage,
+            decimal,
+            decimalFixed,
+            type
+        } = this.props;
+
+        let classNames = className ? [className] : [];
         classNames.push('progress-bar');
-
-        let value = this.props.value;
-        if (value < 0) {
-            value = 0;
+        if (this.children.length) {
+            classNames.push('progress-bar-content');
         }
-        if (value > 100) {
-            value = 100;
-        }
-        let style = 'width: ' + value + '%';
 
-        let title = this.props.title || value + '%'
+        min = parseNumber(min);
+        max = parseNumber(max);
+        let minOrZero = min || 0;
+        let maxOrOne = max || 1;
+
+        if (value < minOrZero) {
+            value = minOrZero;
+        }
+        if (value > maxOrOne) {
+            value = maxOrOne;
+        }
+
+        let percentage = 100 * (value - minOrZero) / (maxOrOne - minOrZero);
+        let text = showPercentage ? numberToPercentage(percentage, decimal, decimalFixed) : undefined;
+
+        switch (type) {
+            case 'success':
+                classNames.push('progress-bar-success');
+                break;
+            case 'info':
+                classNames.push('progress-bar-info');
+                break;
+            case 'warning':
+                classNames.push('progress-bar-warning');
+                break;
+            case 'danger':
+                classNames.push('progress-bar-danger');
+                break;
+        }
+
+        let style = { "--progress-bar-progress": percentage + '%' };
 
         return (
-            <div className={classNames.join(' ')} id={this.props.id}>
-                <div style={style}>
-                    {title}
-                </div>
+            <div
+                id={id}
+                className={classNames.join(' ')}
+                role="progressbar"
+                aria-valuenow={value}
+                aria-valuemin={min}
+                aria-valuemax={max}
+                aria-valuetext={text}
+                style={style as any}
+            >
+                {this.children ?
+                    <div>
+                        {this.children}
+                    </div> :
+                    undefined}
             </div>
         );
+    }
+}
+
+function parseNumber(value: string | number) {
+    if (typeof value === 'number') {
+        return value;
+    } else {
+        value = parseFloat(value);
+        if (!isNaN(value) && isFinite(value)) {
+            return value;
+        } else {
+            return undefined;
+        }
+    }
+}
+
+function numberToPercentage(value: number, decimals: number, fixed: boolean) {
+    if (fixed || !Number.isInteger(value)) {
+        return value.toFixed(decimals || 0) + '%';
+    } else {
+        return value + '%';
     }
 }
--- src/tests/demo/scripts/views/progressBar/ProgressBarView.tsx
+++ src/tests/demo/scripts/views/progressBar/ProgressBarView.tsx
@@ -10,7 +10,10 @@ export default class ProgressBarView extends Component<IProgressBarViewProps> {
     render() {
         return (
             <Section header="Progress Bar" space>
-                <ProgressBar value={50} />
+                <h3>Default</h3>
+                <ProgressBar value={50} min={0} max={100} decimal={2} decimalFixed showPercentage />
+                <h3>Success</h3>
+                <ProgressBar value={50} min={0} max={100} decimal={2} decimalFixed showPercentage type="success" />
             </Section>
         );
     }
