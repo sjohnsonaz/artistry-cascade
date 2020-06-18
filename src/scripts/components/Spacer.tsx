fece6cd7e014986967e13e87e558091011099d7e
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-06-18 02:48:03 -0700

    Added list prop to Spacer.

--- src/scripts/components/Spacer.tsx
+++ src/scripts/components/Spacer.tsx
@@ -1,16 +1,28 @@
 import Cascade, { Component } from 'cascade';
+import ClassNames from '../util/ClassNames';
 
 export interface ISpacerProps {
     className?: string;
     id?: string;
+    list?: boolean;
 }
 
 export default class Spacer extends Component<ISpacerProps> {
     render() {
-        let classNames = this.props.className ? [this.props.className] : [];
-        classNames.push('spacer');
-        return (
-            <li className={classNames.join(' ')} id={this.props.id}></li>
-        );
+        let {
+            className,
+            id,
+            list
+        } = this.props;
+        let classNames = new ClassNames(className, 'spacer');
+        if (list) {
+            return (
+                <li className={classNames.toString()} id={id}></li>
+            );
+        } else {
+            return (
+                <div className={classNames.toString()} id={id}></div>
+            );
+        }
     }
 }
